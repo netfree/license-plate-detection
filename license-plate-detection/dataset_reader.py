d83d3fc32566fe109f609d1327732d4c05c28697
Andrei Muntean <andreimuntean909@gmail.com>
2021-11-23 18:35:29 +0200

    added docu

--- license-plate-detection/dataset_reader.py
+++ license-plate-detection/dataset_reader.py
@@ -8,8 +8,20 @@ class DatasetGateway(object):
         pass
 
     @staticmethod
-    def get_label(self, path):
-        pass
+    def get_label(path) -> Tuple[int, int, int, int]:
+        """
+        Reads the position of the license plate from the label file
+        :param path: label file path
+        :return: [int, int, int, int]
+        """
+        with open(path) as f:
+            for line_number, line in enumerate(f):
+                if line_number == 7:
+                    line = line.strip(" \n")
+                    lhs, rhs = line.split(":")
+                    coords = rhs.split(" ")[1:]
+                    coords = [int(x) for x in coords]
+        return coords
 
     def get_image(self):
         pass
@@ -26,8 +38,8 @@ class DatasetGateway(object):
             for file in os.listdir(os.path.join(self.db_path, image_folder)):
                 if file.endswith(".png"):
                     basename = file[:-4]
-                    label_path = os.path.join(self.db_path, basename+".txt")
-                    image_path = os.path.join(self.db_path, basename+".png")
+                    label_path = os.path.join(os.path.join(self.db_path, image_folder), basename + ".txt")
+                    image_path = os.path.join(os.path.join(self.db_path, image_folder), basename + ".png")
                     data.append(image_path)
                     labels.append(label_path)
 
@@ -37,3 +49,4 @@ class DatasetGateway(object):
 dataset_gateway = DatasetGateway(r"D:\UFPR-ALPR dataset\testing")
 data, labels = dataset_gateway.get_all_paths()
 labels = [DatasetGateway.get_label(path) for path in labels]
+print(len(labels))
